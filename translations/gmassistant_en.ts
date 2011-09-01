3f80a7c66e933b8cf349906db71aae3323e563f8
ViviCoder <vinceprat@free.fr>
2011-09-01 10:43:58 +0200

    New strings translated

--- translations/gmassistant_en.ts
+++ translations/gmassistant_en.ts
@@ -4,44 +4,124 @@
 <context>
     <name>mainWindow</name>
     <message>
-        <location filename="../sources/MainWindow.ui" line="6"/>
+        <location filename="../sources/MainWindow.ui" line="14"/>
         <source>GM-Assistant</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../sources/MainWindow.ui" line="15"/>
+        <location filename="../sources/MainWindow.ui" line="23"/>
         <source>Scenario</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../sources/MainWindow.ui" line="33"/>
+        <location filename="../sources/MainWindow.ui" line="41"/>
         <source>History</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../sources/MainWindow.ui" line="45"/>
+        <location filename="../sources/MainWindow.ui" line="53"/>
         <source>Notes</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../sources/MainWindow.ui" line="57"/>
+        <location filename="../sources/MainWindow.ui" line="65"/>
         <source>Playing characters</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../sources/MainWindow.ui" line="69"/>
+        <location filename="../sources/MainWindow.ui" line="77"/>
         <source>Sound effects</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../sources/MainWindow.ui" line="80"/>
-        <location filename="../sources/MainWindow.ui" line="110"/>
+        <location filename="../sources/MainWindow.ui" line="88"/>
+        <location filename="../sources/MainWindow.ui" line="118"/>
         <source>Play</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../sources/MainWindow.ui" line="99"/>
+        <location filename="../sources/MainWindow.ui" line="107"/>
         <source>Music</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="149"/>
+        <source>&amp;Game</source>
+        <translation>&amp;Game</translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="164"/>
+        <source>&amp;New</source>
+        <translation>&amp;New</translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="167"/>
+        <source>Create a new game</source>
+        <translation>Create a new game</translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="170"/>
+        <source>Ctrl+N</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="175"/>
+        <source>&amp;Load</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="178"/>
+        <source>Load a game</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="181"/>
+        <source>Ctrl+O</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="186"/>
+        <source>&amp;Save</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="189"/>
+        <source>Save the current game</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="192"/>
+        <source>Ctrl+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="197"/>
+        <source>S&amp;ave as</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="200"/>
+        <source>Save the current game in a new file</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="203"/>
+        <source>Ctrl+Shift+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="208"/>
+        <source>&amp;Quit</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="211"/>
+        <source>Exit the application</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="214"/>
+        <source>Ctrl+Q</source>
         <translation></translation>
     </message>
 </context>
--- translations/gmassistant_fr.ts
+++ translations/gmassistant_fr.ts
@@ -4,45 +4,125 @@
 <context>
     <name>mainWindow</name>
     <message>
-        <location filename="../sources/MainWindow.ui" line="6"/>
+        <location filename="../sources/MainWindow.ui" line="14"/>
         <source>GM-Assistant</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../sources/MainWindow.ui" line="15"/>
+        <location filename="../sources/MainWindow.ui" line="23"/>
         <source>Scenario</source>
         <translation>Scénario</translation>
     </message>
     <message>
-        <location filename="../sources/MainWindow.ui" line="33"/>
+        <location filename="../sources/MainWindow.ui" line="41"/>
         <source>History</source>
         <translation>Historique</translation>
     </message>
     <message>
-        <location filename="../sources/MainWindow.ui" line="45"/>
+        <location filename="../sources/MainWindow.ui" line="53"/>
         <source>Notes</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../sources/MainWindow.ui" line="57"/>
+        <location filename="../sources/MainWindow.ui" line="65"/>
         <source>Playing characters</source>
         <translation>Personnages joueurs</translation>
     </message>
     <message>
-        <location filename="../sources/MainWindow.ui" line="69"/>
+        <location filename="../sources/MainWindow.ui" line="77"/>
         <source>Sound effects</source>
         <translation>Bruitages</translation>
     </message>
     <message>
-        <location filename="../sources/MainWindow.ui" line="80"/>
-        <location filename="../sources/MainWindow.ui" line="110"/>
+        <location filename="../sources/MainWindow.ui" line="88"/>
+        <location filename="../sources/MainWindow.ui" line="118"/>
         <source>Play</source>
         <translation>Jouer</translation>
     </message>
     <message>
-        <location filename="../sources/MainWindow.ui" line="99"/>
+        <location filename="../sources/MainWindow.ui" line="107"/>
         <source>Music</source>
         <translation>Musique</translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="149"/>
+        <source>&amp;Game</source>
+        <translation>&amp;Jeu</translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="164"/>
+        <source>&amp;New</source>
+        <translation>&amp;Nouveau</translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="167"/>
+        <source>Create a new game</source>
+        <translation>Crée un nouveau jeu</translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="170"/>
+        <source>Ctrl+N</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="175"/>
+        <source>&amp;Load</source>
+        <translation>&amp;Charger</translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="178"/>
+        <source>Load a game</source>
+        <translation>Charge un jeu</translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="181"/>
+        <source>Ctrl+O</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="186"/>
+        <source>&amp;Save</source>
+        <translation>&amp;Sauvegarder</translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="189"/>
+        <source>Save the current game</source>
+        <translation>Sauvegarde le jeu en cours</translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="192"/>
+        <source>Ctrl+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="197"/>
+        <source>S&amp;ave as</source>
+        <translation>S&amp;auvegarder sous</translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="200"/>
+        <source>Save the current game in a new file</source>
+        <translation>Sauvegarde le jeu en cours dans un nouveau fichier</translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="203"/>
+        <source>Ctrl+Shift+S</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="208"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Quitter</translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="211"/>
+        <source>Exit the application</source>
+        <translation>Sort de l&apos;application</translation>
+    </message>
+    <message>
+        <location filename="../sources/MainWindow.ui" line="214"/>
+        <source>Ctrl+Q</source>
+        <translation></translation>
     </message>
 </context>
 </TS>
