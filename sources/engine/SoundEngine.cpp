27fb12ee6746469cedce7abc3b2e3038c5376e3a
ViviCoder <vinceprat@free.fr>
2011-09-17 17:49:18 +0200

    Replacement of FMOD by SDL_mixer (not playing sound yet)

--- sources/engine/SoundEngine.cpp
+++ sources/engine/SoundEngine.cpp
@@ -4,85 +4,32 @@
 using namespace std;
 
 // Constructor
-SoundEngine::SoundEngine(): currentsound(0), lenms(0)
+SoundEngine::SoundEngine() throw(runtime_error): music(NULL), sound(NULL)
 {
-    sPath = "../data/sound/";
-    /*
-     * Create a system object and initialize.
-    */
-    result = FMOD::System_Create(&system);
-    errcheck(result);
+    SDL_Init(SDL_INIT_AUDIO);
 
-    result = system->setOutput(FMOD_OUTPUTTYPE_ALSA);
-    errcheck(result);
+    // set up
+    int audio_rate = 22050;
+    Uint16 audio_format = AUDIO_S16; 
+    int audio_channels = 2;
+    int audio_buffers = 4096;
 
-    result = system->init(32, FMOD_INIT_NORMAL, 0);
-    errcheck(result);
-}
-
-SoundEngine::SoundEngine(std::string &filename): currentsound(0), lenms(0)
-{
-    sFilename = filename;
-    sPath = "../data/sound/";
-    /*
-     * Create a system object and initialize.
-    */
-    result = FMOD::System_Create(&system);
-    errcheck(result);
-
-    result = system->setOutput(FMOD_OUTPUTTYPE_ALSA);
-    errcheck(result);
-
-    result = system->init(32, FMOD_INIT_NORMAL, 0);
-    errcheck(result);
-
-//  result = system->createSound("cri.mp3", FMOD_SOFTWARE, &cri);
-    errcheck(result);
-
-    result = system->createSound(sFilename.c_str(), FMOD_SOFTWARE, 0,&sound);
-    errcheck(result);
-
-    sound->getLength(&lenms, FMOD_TIMEUNIT_MS);
-    cout << "Duree totale: "<< lenms << " ms" << endl;
-
-}
-
-// Methods
-//
-void SoundEngine::errcheck(FMOD_RESULT result)
-{
-    if (result != FMOD_OK)
+    if(Mix_OpenAudio(audio_rate, audio_format, audio_channels, audio_buffers))
     {
-        //cout << "FMOD error! (" << result << ") " << FMOD_ErrorString(result) <<endl;
-        //exit(-1);
-        throw std::runtime_error(FMOD_ErrorString(result));
+        throw runtime_error("Unable to open the audio device");        
     }
 }
 
-void SoundEngine::playSound(const std::string &filename)
-{
-    sFilename = filename;
-    
-    //cout << "File tried: " << sFilename << endl;
-
-    result = system->createSound(sFilename.c_str(), FMOD_SOFTWARE, 0,&sound);
-    errcheck(result);
-
-    sound->getLength(&lenms, FMOD_TIMEUNIT_MS);
-    cout << "Duree totale: "<< lenms << " ms" << endl;
-
-    result = system->playSound(FMOD_CHANNEL_FREE, sound, 0, &channel);
-    errcheck(result);
-    Sleep(lenms);
-    
-    result = sound->release();
-    errcheck(result);
-}
+// destructor
 
 SoundEngine::~SoundEngine()
 {
-    result = system->close();
-    errcheck(result);
-    result = system->release();
-    errcheck(result);
+    Mix_CloseAudio();
+    SDL_Quit();
+}
+
+// Methods
+
+void SoundEngine::playSound(const std::string &filename)
+{
 }
--- sources/engine/SoundEngine.h
+++ sources/engine/SoundEngine.h
@@ -1,37 +1,25 @@
 #ifndef HEADER_SOUNDENGINE
 #define HEADER_SOUNDENGINE
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <iostream>
-#include "fmod.hpp"
-#include "fmod_errors.h"
-#include "wincompat.h"
+#include <SDL.h>
+#include <SDL_mixer.h>
 #include <string>
+#include <stdexcept>
 
 class SoundEngine
 {
 	private:
-		std::string sFilename, sPath;
-        FMOD::System    *system;
-        FMOD::Sound     *sound;
-        FMOD::Channel   *channel;
-        FMOD_RESULT     result;
-        int             key;
-        unsigned int    version;
-        FMOD::Sound *currentsound;
-        unsigned int    lenms;
+        Mix_Music *music;
+        Mix_Chunk *sound;
 
 	public:
 		// constructor
-		SoundEngine();
-		SoundEngine(std::string &filename);
+		SoundEngine() throw(std::runtime_error);
         // destructor
         ~SoundEngine();
 		// accessors
 		void playSound(const std::string &filename);
-		void errcheck(FMOD_RESULT result);
-        void clear();
+//		void playMusic(const std::string &filename);
 };
 
 #endif
