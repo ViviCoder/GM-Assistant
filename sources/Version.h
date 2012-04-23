af8d7caf92c00715c3d58c31dfe7efb14bceaf2f
ViviCoder <vinceprat@free.fr>
2012-04-23 19:17:01 +0200

    Oups! Files forgotten...

---
+++ sources/Version.h
@@ -0,0 +1,61 @@
+/*************************************************************************
+* Copyright © 2011-2012 Vincent Prat & Simon Nicolas
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program; if not, write to the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*************************************************************************/
+
+#ifndef HEADER_VERSION
+#define HEADER_VERSION
+
+#define MAJOR_VERSION   1
+#define MINOR_VERSION   2
+#define RELEASE_VERSION 0
+
+// some stringification macros
+#define xstr(s) str(s)
+#define str(s)  #s
+
+// version string
+#define SHORT_VERSION   xstr(MAJOR_VERSION)"."xstr(MINOR_VERSION)
+#define FULL_VERSION    SHORT_VERSION"."xstr(RELEASE_VERSION)
+
+/*!
+ * \brief Class containing version information
+ */
+class Version
+{
+    private:
+        //! \brief Major version number
+        int iMajor;
+        //! \brief Minor version number
+        int iMinor;
+        //! \brief Release version number
+        int iRelease;
+    public:
+        /*!
+         * \brief Constructor
+         * \param major Major version number
+         * \param minor Minor version number
+         * \param release Release version number
+         */
+        Version(int major = 1, int minor = 0, int release = 0);
+        /*!
+         * \brief Current version
+         * \return Current version
+         */
+        static Version currentVersion();
+};
+
+#endif
