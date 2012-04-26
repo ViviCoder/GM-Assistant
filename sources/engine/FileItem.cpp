f327b68b6e9ce4f5d22dd823c5e1a57edd71c22d
ViviCoder <vinceprat@free.fr>
2012-04-26 16:29:12 +0200

    Forgot to update copyright information

--- sources/engine/FileItem.cpp
+++ sources/engine/FileItem.cpp
@@ -1,5 +1,5 @@
 /*************************************************************************
-* Copyright © 2011 Vincent Prat & Simon Nicolas
+* Copyright © 2011-2012 Vincent Prat & Simon Nicolas
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
