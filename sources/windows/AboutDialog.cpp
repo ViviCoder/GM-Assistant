588de6e22581b68f9d730b44532456d54a782d5c
ViviCoder <vinceprat@free.fr>
2012-06-07 11:42:53 +0200

    Issue #57 solved

--- sources/windows/AboutDialog.cpp
+++ sources/windows/AboutDialog.cpp
@@ -19,12 +19,15 @@
 #include "AboutDialog.h"
 #include "version.h"
 #include <QMessageBox>
+#include <QSvgRenderer>
 
 AboutDialog::AboutDialog(QWidget *parent): QDialog(parent)
 {
     setupUi(this);
     labelVersionNumber->setText(FULL_VERSION);
-    labelLogo->load(QString(":data/images/GMA.svg"));
+    QSvgRenderer *renderer = labelLogo->renderer();
+    renderer->load(QString(":data/images/GMA.svg"));
+    labelLogo->setFixedSize(labelLogo->renderer()->defaultSize());
 }
 
 void AboutDialog::showEvent(QShowEvent *e)
