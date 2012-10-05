ViviCoder/GM-Assistant

commit 68c022861d841f26e1db6f9a1f23ba9737ae6428

Issue #63: test of un(re)doability before using the shortcut

sources/windows/MainWindow.cpp
@@ -672,11 +672,11 @@ bool MainWindow::eventFilter(QObject *source, QEvent *e)
         if (event->key() == Qt::Key_Z)
         {
             Qt::KeyboardModifiers modifiers = event->modifiers();
-            if (modifiers == Qt::ControlModifier)
+            if (modifiers == Qt::ControlModifier && mqQueue.undoable())
             {
                 on_action_Undo_triggered();
             }
-            else if (modifiers == (Qt::ControlModifier | Qt::ShiftModifier))
+            else if (modifiers == (Qt::ControlModifier | Qt::ShiftModifier) && mqQueue.redoable())
             {
                 on_action_Redo_triggered();
             }

commit 4abb0505bf386a74f8936f6ea14439253b4fdc6d

Issue #63: correction of a bug introduced by 68c022861d841f26e1db6f9a1f23ba9737ae6428 (impossible to undo or redo before focusing out of the note editor)

sources/widgets/QCustomTextEdit.cpp
@@ -47,6 +47,15 @@ void QCustomTextEdit::onTextChanged()
     }
 }
 
+void QCustomTextEdit::checkModification()
+{
+    QString sText = toPlainText();
+    if (pNotes && sRef != sText)
+    {
+        emit modificationDone(new NoteModification(*pNotes, sRef.toStdString(), sText.toStdString()));
+    }
+}
+
 void QCustomTextEdit::focusInEvent(QFocusEvent *e)
 {
     sRef = toPlainText();
@@ -55,10 +64,6 @@ void QCustomTextEdit::focusInEvent(QFocusEvent *e)
 
 void QCustomTextEdit::focusOutEvent(QFocusEvent *e)
 {
-    QString sText = toPlainText();
-    if (pNotes && sRef != sText)
-    {
-        emit modificationDone(new NoteModification(*pNotes, sRef.toStdString(), sText.toStdString()));
-    }
+    checkModification();
     QTextEdit::focusOutEvent(e);
 }

sources/widgets/QCustomTextEdit.h
@@ -45,6 +45,12 @@ class QCustomTextEdit: public QTextEdit
          * \brief Update of the display
          */
         void updateDisplay();
+        /*!
+         * \brief Modification checking
+         *
+         * Checks if the text has been modified and send the corresponding signal if it is the case
+         */
+        void checkModification();
     protected:
         /*!
          * \brief FocusIn event handler
@@ -57,7 +63,7 @@ class QCustomTextEdit: public QTextEdit
          * \brief FocusOut event handler
          * \param e Event to handle
          *
-         * Checks if the text has been modified and send the corresponding signal if it is the case
+         * Checks if the text has been modified before releasing focus
          */
         void focusOutEvent(QFocusEvent *e);
     protected slots:

sources/windows/MainWindow.cpp
@@ -251,7 +251,6 @@ void MainWindow::on_action_Load_triggered()
 
 void MainWindow::on_action_Save_triggered()
 {
-    setFocus();
     if (sFileName.isEmpty())
     {
         on_actionS_ave_as_triggered();
@@ -608,7 +607,6 @@ void MainWindow::on_sliderMusic_wheeled(bool positive)
 
 void MainWindow::on_action_Undo_triggered()
 {
-    setFocus();
     updateModification(mqQueue.undo());
     updateUndoRedo();
 }
@@ -650,7 +648,6 @@ void MainWindow::updateModification(Modification *modification)
 
 void MainWindow::on_action_Redo_triggered()
 {
-    setFocus();
     updateModification(mqQueue.redo());
     updateUndoRedo();
 }
@@ -675,13 +672,21 @@ bool MainWindow::eventFilter(QObject *source, QEvent *e)
         if (event->key() == Qt::Key_Z)
         {
             Qt::KeyboardModifiers modifiers = event->modifiers();
-            if (modifiers == Qt::ControlModifier && mqQueue.undoable())
+            if (modifiers == Qt::ControlModifier)
             {
-                on_action_Undo_triggered();
+                textNotes->checkModification();
+                if (mqQueue.undoable())
+                {
+                    on_action_Undo_triggered();
+                }
             }
-            else if (modifiers == (Qt::ControlModifier | Qt::ShiftModifier) && mqQueue.redoable())
+            else if (modifiers == (Qt::ControlModifier | Qt::ShiftModifier))
             {
-                on_action_Redo_triggered();
+                textNotes->checkModification();
+                if (mqQueue.redoable())
+                {
+                    on_action_Redo_triggered();
+                }
             }
         }
     }

translations/gmassistant_fr.ts
@@ -1084,8 +1084,8 @@ p, li { white-space: pre-wrap; }
     </message>
     <message>
         <location filename="../sources/windows/MainWindow.ui" line="247"/>
-        <location filename="../sources/windows/MainWindow.cpp" line="394"/>
-        <location filename="../sources/windows/MainWindow.cpp" line="423"/>
+        <location filename="../sources/windows/MainWindow.cpp" line="393"/>
+        <location filename="../sources/windows/MainWindow.cpp" line="422"/>
         <source>&amp;Play</source>
         <translation>&amp;Lecture</translation>
     </message>
@@ -1350,13 +1350,13 @@ p, li { white-space: pre-wrap; }
         <translation type="obsolete">Reprise</translation>
     </message>
     <message>
-        <location filename="../sources/windows/MainWindow.cpp" line="356"/>
-        <location filename="../sources/windows/MainWindow.cpp" line="438"/>
+        <location filename="../sources/windows/MainWindow.cpp" line="355"/>
+        <location filename="../sources/windows/MainWindow.cpp" line="437"/>
         <source>&amp;Pause</source>
         <translation>&amp;Pause</translation>
     </message>
     <message>
-        <location filename="../sources/windows/MainWindow.cpp" line="362"/>
+        <location filename="../sources/windows/MainWindow.cpp" line="361"/>
         <source>&amp;Resume</source>
         <translation>&amp;Reprise</translation>
     </message>
@@ -1366,14 +1366,14 @@ p, li { white-space: pre-wrap; }
         <location filename="../sources/widgets/QCustomTreeWidget.cpp" line="461"/>
         <location filename="../sources/windows/MainWindow.cpp" line="235"/>
         <location filename="../sources/windows/MainWindow.cpp" line="239"/>
-        <location filename="../sources/windows/MainWindow.cpp" line="269"/>
-        <location filename="../sources/windows/MainWindow.cpp" line="298"/>
-        <location filename="../sources/windows/MainWindow.cpp" line="444"/>
-        <location filename="../sources/windows/MainWindow.cpp" line="449"/>
-        <location filename="../sources/windows/MainWindow.cpp" line="461"/>
-        <location filename="../sources/windows/MainWindow.cpp" line="487"/>
-        <location filename="../sources/windows/MainWindow.cpp" line="498"/>
-        <location filename="../sources/windows/MainWindow.cpp" line="502"/>
+        <location filename="../sources/windows/MainWindow.cpp" line="268"/>
+        <location filename="../sources/windows/MainWindow.cpp" line="297"/>
+        <location filename="../sources/windows/MainWindow.cpp" line="443"/>
+        <location filename="../sources/windows/MainWindow.cpp" line="448"/>
+        <location filename="../sources/windows/MainWindow.cpp" line="460"/>
+        <location filename="../sources/windows/MainWindow.cpp" line="486"/>
+        <location filename="../sources/windows/MainWindow.cpp" line="497"/>
+        <location filename="../sources/windows/MainWindow.cpp" line="501"/>
         <source>Error</source>
         <translation>Erreur</translation>
     </message>
@@ -1384,34 +1384,34 @@ p, li { white-space: pre-wrap; }
     </message>
     <message>
         <location filename="../sources/windows/MainWindow.cpp" line="224"/>
-        <location filename="../sources/windows/MainWindow.cpp" line="276"/>
+        <location filename="../sources/windows/MainWindow.cpp" line="275"/>
         <source>GM-Assistant files (*.gma);;XML files (*.xml)</source>
         <translation>Fichiers GM-Assistant (*.gma);;Fichiers XML (*.xml)</translation>
     </message>
     <message>
         <location filename="../sources/windows/MainWindow.cpp" line="239"/>
-        <location filename="../sources/windows/MainWindow.cpp" line="502"/>
+        <location filename="../sources/windows/MainWindow.cpp" line="501"/>
         <source>The game will be loaded anyway, but some features might not work properly.</source>
         <translation>Le jeu va tout de même être chargé, mais certaines fonctionnalités peuvent ne pas fonctionner normalement.</translation>
     </message>
     <message>
         <location filename="../sources/windows/MainWindow.cpp" line="239"/>
-        <location filename="../sources/windows/MainWindow.cpp" line="502"/>
+        <location filename="../sources/windows/MainWindow.cpp" line="501"/>
         <source>The game cannot be loaded correctly for the following reason: </source>
         <translation>Le jeu n&apos;a pas pu être chargé pour la raison suivante :</translation>
     </message>
     <message>
-        <location filename="../sources/windows/MainWindow.cpp" line="276"/>
+        <location filename="../sources/windows/MainWindow.cpp" line="275"/>
         <source>Select the file to save</source>
         <translation>Sélectionnez le fichier à sauvegarder</translation>
     </message>
     <message>
-        <location filename="../sources/windows/MainWindow.cpp" line="449"/>
+        <location filename="../sources/windows/MainWindow.cpp" line="448"/>
         <source>Unable to play the file</source>
         <translation>Impossible de jouer le fichier</translation>
     </message>
     <message>
-        <location filename="../sources/windows/MainWindow.cpp" line="487"/>
+        <location filename="../sources/windows/MainWindow.cpp" line="486"/>
         <source>The file &quot;%1&quot; does not exist.</source>
         <translation>Le fichier &quot;%1&quot; n&apos;existe pas.</translation>
     </message>
@@ -1424,7 +1424,7 @@ p, li { white-space: pre-wrap; }
         <translation type="obsolete">Le fichier n&apos;existe pas.</translation>
     </message>
     <message>
-        <location filename="../sources/windows/MainWindow.cpp" line="547"/>
+        <location filename="../sources/windows/MainWindow.cpp" line="546"/>
         <source>New game</source>
         <translation>Nouveau jeu</translation>
     </message>