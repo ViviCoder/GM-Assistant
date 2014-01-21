ViviCoder/GM-Assistant

commit acda2687c125f62110bfd20242ab9039f2d062e1

Issue #64: modification of the tool tip for included files

sources/widgets/QCustomTreeWidgetItem.cpp
@@ -1,5 +1,5 @@
 /*************************************************************************
-* Copyright © 2011-2012 Vincent Prat & Simon Nicolas
+* Copyright © 2011-2013 Vincent Prat & Simon Nicolas
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -49,22 +49,32 @@ void QCustomTreeWidgetItem::updateDisplay()
     Item *item = pBranch->item();
     setText(0,item->content().c_str());
     setIcon(1,QCustomTreeWidget::icon(item->state()));
-    switch (item->type())
+    Item::Type type = item->type();
+    if (Item::is(type, Item::tFile))
     {
-        case Item::tSound:  setIcon(0,QIcon(":/data/images/speaker.svg"));
-                            setToolTip(0,dynamic_cast<SoundItem*>(item)->fileName().c_str()); 
-                            if (dynamic_cast<QCustomTreeWidget*>(treeWidget())->playingMethod() != QCustomTreeWidget::pmNone)
-                            {
-                                setStatusTip(0,QApplication::translate("customTree","Double click to play the file",0));
-                            }
-                            break;
-        case Item::tImage:  setIcon(0,QIcon(":/data/images/image.svg"));
-                            setToolTip(0,dynamic_cast<ImageItem*>(item)->fileName().c_str()); 
-                            setStatusTip(0,QApplication::translate("customTree","Double click to show the file",0));
-                            break;
-        default:    setIcon(0, QIcon());
-                    setToolTip(0, "");
-                    setStatusTip(0, "");
-                    break;
+        FileItem *fileItem = dynamic_cast<FileItem*>(item);
+        std::string fileName = fileItem->fileName(); 
+        if (fileItem->isIncluded())
+        {
+            QFileInfo file(fileName.c_str());
+            setToolTip(0, file.fileName() + QApplication::translate("customTree", " (included in the loaded file)", 0));
+        }
+        else
+        {
+            setToolTip(0, fileName.c_str()); 
+        }
+        switch (type)
+        {
+            case Item::tSound:  setIcon(0,QIcon(":/data/images/speaker.svg"));
+                                if (dynamic_cast<QCustomTreeWidget*>(treeWidget())->playingMethod() != QCustomTreeWidget::pmNone)
+                                {
+                                    setStatusTip(0,QApplication::translate("customTree","Double click to play the file",0));
+                                }
+                                break;
+            case Item::tImage:  setIcon(0,QIcon(":/data/images/image.svg"));
+                                setStatusTip(0,QApplication::translate("customTree","Double click to show the file",0));
+                                break;
+            default:    break;
+        }
     }
 }

sources/engine/items/FileItem.cpp
@@ -22,7 +22,7 @@
 
 using namespace std;
 
-FileItem::FileItem(const string &content, State state, bool expanded, const string &fileName): Item(content,state,expanded), sFileName(fileName)
+FileItem::FileItem(const string &content, State state, bool expanded, const string &fileName): Item(content,state,expanded), sFileName(fileName), bIncluded(false)
 {
     if (fileName != "")
     {
@@ -61,6 +61,7 @@ void FileItem::fromXML(const IOConfig &config, const xmlpp::Element &root, bool 
     }
     if (config.isArchived())
     {
+        bIncluded = true;
         name = config.temporaryDirectory() + subdirectory() + name;
     }
     setFileName(name, checkFile);

sources/engine/items/FileItem.h
@@ -32,6 +32,8 @@ class FileItem: public Item
          * \brief Name of the file related to the item
          */
         std::string sFileName;
+        //! Flag if included in archive or not
+        bool bIncluded;
     public:
         /*!
          * \brief Setter for the name of the file of the item
@@ -75,6 +77,11 @@ class FileItem: public Item
          * \return Subdirectory of the archive in which the file is stored
          */
         virtual std::string subdirectory() const;
+        /*!
+         * \brief Getter for the inclusion flag
+         * \return True if the item is included in an archive file, false otherwise
+         */
+        bool isIncluded() const;
 };
 
 inline Item::Type FileItem::type() const
@@ -90,6 +97,11 @@ inline std::string FileItem::fileName() const
 inline std::string FileItem::subdirectory() const
 {
     return "misc/";
+}
+
+inline bool FileItem::isIncluded() const
+{
+    return bIncluded;
 }
 
 #endif

translations/gmassistant_fr.ts
@@ -425,12 +425,17 @@ p, li { white-space: pre-wrap; }
         <translation>A&amp;fficher</translation>
     </message>
     <message>
-        <location filename="../sources/widgets/QCustomTreeWidgetItem.cpp" line="58"/>
+        <location filename="../sources/widgets/QCustomTreeWidgetItem.cpp" line="60"/>
+        <source> (included in the loaded file)</source>
+        <translation> (inclus dans le fichier chargé)</translation>
+    </message>
+    <message>
+        <location filename="../sources/widgets/QCustomTreeWidgetItem.cpp" line="71"/>
         <source>Double click to play the file</source>
         <translation>Double-cliquez pour jouer le fichier</translation>
     </message>
     <message>
-        <location filename="../sources/widgets/QCustomTreeWidgetItem.cpp" line="63"/>
+        <location filename="../sources/widgets/QCustomTreeWidgetItem.cpp" line="75"/>
         <source>Double click to show the file</source>
         <translation>Double-cliquez pour afficher l&apos;image</translation>
     </message>

commit ba6e2038558924e57c8cf3b0f5dfa77d8e4e53f3

Fixes a bug introduced by commit acda2687c125f62110bfd20242ab9039f2d062e1

sources/widgets/QCustomTreeWidgetItem.cpp
@@ -1,5 +1,5 @@
 /*************************************************************************
-* Copyright © 2011-2013 Vincent Prat & Simon Nicolas
+* Copyright © 2011-2014 Vincent Prat & Simon Nicolas
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -76,5 +76,11 @@ void QCustomTreeWidgetItem::updateDisplay()
                                 break;
             default:    break;
         }
+    }
+    else
+    {
+        setIcon(0, QIcon());
+        setStatusTip(0, "");
+        setToolTip(0, ""); 
     }
 }

sources/widgets/QCustomTreeWidgetItem.h
@@ -1,5 +1,5 @@
 /*************************************************************************
-* Copyright © 2011 Vincent Prat & Simon Nicolas
+* Copyright © 2011-2014 Vincent Prat & Simon Nicolas
 *
 * This program is free software; you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
@@ -33,7 +33,9 @@ class QCustomTreeWidgetItem: public QTreeWidgetItem
         QCustomTreeWidgetItem(QCustomTreeWidget *parent, Branch *branch, QCustomTreeWidgetItem *previous);
         QCustomTreeWidgetItem(QCustomTreeWidgetItem *parent, Branch *branch, QCustomTreeWidgetItem *previous);
         Branch* branch();
-        // update the displat (text and icons)
+        /*
+         * \brief Update of the display (icon, text, tool/status tips)
+         */
         void updateDisplay();
 };
 